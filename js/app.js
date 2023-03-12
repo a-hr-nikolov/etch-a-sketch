3927b1c622034de1cc5cbb07d51fa46473529b09
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-12 13:56:30 +0200

    Refactor createGrid function to take dynamic grid size

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
 const container = document.querySelector('.grid-container');
-const gridBox = [];
+const gridItem = [];
 
-for (let i = 0; i < 256; i++) {
-  gridBox[i] = document.createElement('div');
-  gridBox[i].classList.add('gridbox');
-  // gridBox[i].style.cssText =
-  //   'width: 50px; height: 50px; background: pink; margin: 0; padding: 0; border: 1px solid black; box-sizing: border-box;';
-  container.appendChild(gridBox[i]);
-  // gridBox[i].addEventListener('mouseenter');
+function changeBoxColor(event) {
+  event.target.style.background = 'black';
 }
+
+function createGrid(gridSize) {
+  let gridItemSize = parseFloat(container.style.width) / gridSize;
+  console.log(gridItemSize);
+  for (let i = 0; i < gridSize ** 2; i++) {
+    gridItem[i] = document.createElement('div');
+    gridItem[i].classList.add('grid-item');
+    gridItem[i].style.width = `${gridItemSize}px`;
+    gridItem[i].style.height = `${gridItemSize}px`;
+    container.appendChild(gridItem[i]);
+    gridItem[i].addEventListener('mouseenter', changeBoxColor);
+  }
+}
+
+function clearGrid() {
+  container.innerHTML = '';
+}
+
+function setGrid() {
+  let newGridSize = parseInt(document.querySelector('#gridsize'));
+  if (newGridSize > 100) newGridSize = 100;
+  clearGrid();
+  createGrid(newGridSize);
+}
+
+createGrid(16);
