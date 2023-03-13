e202fede6f7d9005c9078ff7bdbb1fb380107de7
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-13 15:47:28 +0200

    Work on coloring only on mouse down

--- js/app.js
+++ js/app.js
@@ -2,12 +2,30 @@ const container = document.querySelector('.grid-container');
 const getClearButton = document.querySelector('#cleargrid');
 const slider = document.querySelector('#gridsize');
 const gridItem = [];
+let actionToggle = false;
 
 function changeBoxColor(event) {
+  if (actionToggle) {
+    event.target.style.background = `rgb(${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )})`;
+  }
+}
+
+function changeBoxColorClick(event) {
   event.target.style.background = `rgb(${Math.floor(
     Math.random() * 256
   )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }
+
+function toggleAction() {
+  actionToggle = !actionToggle;
+}
+
+window.addEventListener('mousedown', toggleAction);
+window.addEventListener('mouseup', toggleAction);
 
 function createGrid(gridSize = 16) {
   let gridItemSize = parseFloat(container.style.width) / gridSize;
@@ -17,7 +35,9 @@ function createGrid(gridSize = 16) {
     gridItem[i].style.width = `${gridItemSize}px`;
     gridItem[i].style.height = `${gridItemSize}px`;
     container.appendChild(gridItem[i]);
-    gridItem[i].addEventListener('mouseenter', changeBoxColor);
+    gridItem[i].addEventListener('mousedown', changeBoxColorClick);
+    gridItem[i].addEventListener('mouseover', changeBoxColor);
+    gridItem[i].addEventListener('mouseout', () => (actionToggle = false));
   }
 }
 
