cec7c00a6a9028da1798eaf5b550e81864167714
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-20 08:57:45 +0200

    Remove unnecessary js files

--- js/paintBlack.js
+++
@@ -1,5 +0,0 @@
-export default function paintBlack(event) {
-  if (event.buttons > 0) {
-    event.target.style.background = 'black';
-  }
-}
--- js/paintRainbow.js
+++
@@ -1,9 +0,0 @@
-export default function paintRainbow(event) {
-  if (event.buttons > 0) {
-    event.target.style.background = `rgb(${Math.floor(
-      Math.random() * 256
-    )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
-      Math.random() * 256
-    )})`;
-  }
-}
