218d8fa2d2ae6763eb3b13d2d017144d4a004651
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-21 13:45:37 +0200

    Make the rainbow paint work on touch (with issues)

--- js/paints.js
+++ js/paints.js
@@ -1,11 +1,23 @@
 function paintRainbow(event) {
-  if (event.buttons === 1 || event.type == 'touchmove') {
+  if (event.buttons === 1) {
     event.preventDefault();
     event.target.style.background = `rgb(${Math.floor(
       Math.random() * 256
     )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
       Math.random() * 256
     )})`;
+  }
+  if (event.type == 'touchmove') {
+    event.preventDefault();
+    const touch = event.touches[0];
+    const target = document.elementFromPoint(touch.clientX, touch.clientY);
+    if (target && target.classList.contains('grid-item')) {
+      target.style.background = `rgb(${Math.floor(
+        Math.random() * 256
+      )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+        Math.random() * 256
+      )})`;
+    }
   }
 }
 
