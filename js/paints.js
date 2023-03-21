785180547298227ac1ffc567e18a571813b4dbd0
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-21 14:19:45 +0200

    Add comments

--- js/paints.js
+++ js/paints.js
@@ -1,3 +1,7 @@
+// This file holds all paint mode functions.
+// Since the functions are simple, I preferred to keep them all in one place.
+// Their names are descriptive enough to figure out what does what.
+
 function paintRainbow(event) {
   if (event.buttons === 1) {
     event.preventDefault();
@@ -7,6 +11,10 @@ function paintRainbow(event) {
       Math.random() * 256
     )})`;
   }
+
+  // Attempt at making touch input work
+  // I have not implemented the code with the other functions,
+  // because I do not like how it works.
   if (event.type == 'touchmove') {
     event.preventDefault();
     const touch = event.touches[0];
