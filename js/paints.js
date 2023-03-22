9c8410c850ab92d6236e6737e059d9671551c8a4
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-03-22 09:27:52 +0200

    Extract touch input code into a separate function

--- js/paints.js
+++ js/paints.js
@@ -2,38 +2,35 @@
 // Since the functions are simple, I preferred to keep them all in one place.
 // Their names are descriptive enough to figure out what does what.
 
+function useTouch(event, color) {
+  event.preventDefault();
+  const touch = event.touches[0];
+  const target = document.elementFromPoint(touch.clientX, touch.clientY);
+  if (
+    target &&
+    target.classList.contains('grid-item') &&
+    target.getAttribute('data-flag') === '0'
+  ) {
+    document
+      .querySelectorAll('.grid-item')
+      .forEach(item => item.setAttribute('data-flag', '0'));
+    target.setAttribute('data-flag', '1');
+    target.style.background = color;
+  }
+}
+
 function paintRainbow(event) {
+  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
+
+  // Handle mouse input
   if (event.buttons === 1) {
-    event.preventDefault();
-    event.target.style.background = `rgb(${Math.floor(
-      Math.random() * 256
-    )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
-      Math.random() * 256
-    )})`;
+    event.target.style.background = randomColor;
   }
 
-  // Attempt at making touch input work.
-  // It is a bit sloppy, but it works.
-  if (event.type == 'touchmove') {
-    event.preventDefault();
-    const touch = event.touches[0];
-    const target = document.elementFromPoint(touch.clientX, touch.clientY);
-    if (
-      target &&
-      target.classList.contains('grid-item') &&
-      target.getAttribute('data-flag') === '0'
-    ) {
-      document
-        .querySelectorAll('.grid-item')
-        .forEach(item => item.setAttribute('data-flag', '0'));
-      target.setAttribute('data-flag', '1');
-      target.style.background = `rgb(${Math.floor(
-        Math.random() * 256
-      )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
-        Math.random() * 256
-      )})`;
-    }
-  }
+  // Handle touch input
+  if (event.type == 'touchmove') useTouch(event, randomColor);
 }
 
 function paintBlack(event) {
